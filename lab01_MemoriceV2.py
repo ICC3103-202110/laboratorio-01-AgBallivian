a2cff4a51d0801ef1d11e65e7e2fb76d1a8d8ec7
agballivian <agballivian@miuandes.cl>
2021-03-25 22:16:48 -0300

    commit 8

--- lab01_MemoriceV2.py
+++ lab01_MemoriceV2.py
@@ -13,12 +13,9 @@ def playcards(c):
     for j in range(0, (c)): #reemplazo de 0 por numeros, esta es la lista de juego
         cardsplay[0][j] = l[0]
         l.pop(0)
-        #print(cardsplay, "\n")
     for j in range(0, (c)):
         cardsplay[1][j] = l[0]
         l.pop(0)
-        #print(cardsplay, "\n")
-    #print(cardsplay)
     return(cardsplay)
 
 def zerocards(c):
@@ -28,10 +25,10 @@ def zerocards(c):
 def Ui(tab_):
     row = tab_.shape[0]
     col = tab_.shape[1]
-    l1 = ["0"] * col
+    l1 = ["0"] * col #Creo lista string para reemplazar
     l2 = ["0"] * col
     ui = [l1, l2]
-    for i in range(0, row):
+    for i in range(0, row): #Compruebo y reemplazo para la interfaz
         for j in range(0, col):         
             if tab_[i][j] == 0:
                 ui[i][j] = ' █ '
@@ -39,7 +36,7 @@ def Ui(tab_):
                 ui[i][j] = tab_[i][j]
             elif tab_[i][j] == 0.9:
                 ui[i][j] = '  *  '
-    for i in range(0, row):
+    for i in range(0, row): #Mostrar interfaz
         print("\n")
         for j in range(0, col):
             print(ui[i][j], '', end='')
@@ -60,12 +57,10 @@ def play_cards(tab_game, tab_answer, c):
             col1 = int(input())
             #Coordenadas 1
             coord1 = (row1, col1)
-            
             #Comprobar si ya esta seleccionado
             if tab_game[row1, col1] == 0:
                 #Mostrar primera seleccion
                 tab_game[row1, col1] = tab_answer[row1, col1]
-                #BETA =====================================
                 Ui(tab_game)
                 play_turn += 1
             else:
@@ -93,22 +88,19 @@ def play_cards(tab_game, tab_answer, c):
                     #Mostrar segunda seleccion
                     tab_game[row2, col2] = tab_answer[row2, col2]
                     Ui(tab_game)
-                    print("+ + + + + + + + + + + \n+ + + P O I N T + + +\n+ + + + + + + + + + + ") 
+                    print("+ + + + + + + + + + + \n+ + + P O I N T + + +\n+ + + + + + + + + + + \n") 
                     tab_game[row2, col2] = 0.9
                     tab_game[row1, col1] = 0.9
                     return(True, tab_game)
                 elif tab_game[row1, col1] != tab_answer[row2, col2]:
                     #Mostrar segunda seleccion
                     tab_game[row2, col2] = tab_answer[row2, col2]
-                    #BETA 
                     Ui(tab_game)
-                    print("- - - - - - - - - -\n- - - F A I L - - -\n- - - - - - - - - -")  
-                    
+                    print("- - - - - - - - - -\n- - - F A I L - - -\n- - - - - - - - - -\n")  
                     #Reiniciar Tablero
                     tab_game = np.copy(tab_back)
                     return(False, tab_back)
         
-
 c = int(input("How many pairs to play?: "))
 
 #Tablas
@@ -158,5 +150,10 @@ while point_total < point_max - 1:
             tab_game = np.copy(player_result[1])
             player_actual = 1
 #Crear Contador final para dar la win a jugador 1 o 2
-
+if point_p1 > point_p2:
+    print("PLAYER 1 WIN WITH ", point_p1, "POINTS!")
+elif point_p1 < point_p2:
+    print("PLAYER 2 WIN WITH ", point_p2, "POINTS!")
+else:
+    print("TIE!")
 print("GAME OVER")   
